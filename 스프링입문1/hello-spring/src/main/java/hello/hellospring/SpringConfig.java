712ddac5f4e39d46713b7cf243fdf3bdd2a2e12c
DESKTOP-URI21C7\JHG <vndanwl@naver.com>
2022-10-03 18:59:39 +0900

    1끝

--- 스프링입문1/hello-spring/src/main/java/hello/hellospring/SpringConfig.java
+++ 스프링입문1/hello-spring/src/main/java/hello/hellospring/SpringConfig.java
@@ -4,11 +4,21 @@ import hello.hellospring.domain.Member;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
 import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.swing.*;
+
 @Configuration
 public class SpringConfig {
+
+    private final MemberRepository memberRepository;
+
+    @Autowired
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
 
     @Bean
     public Member memberService(){
